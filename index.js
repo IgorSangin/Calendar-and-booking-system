af68a077081c2b0e35e763e1eeed5cbab89213c3
Igor Sangin <sangini@coventry.ac.uk>
2019-10-22 17:40:56 +0100

    Trying to fix the code

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ var app = new Koa();
 const cors = require('@koa/cors');
 
 var main = require('./routes/main.js');
-var admin = require('./routes/admin');
+var admin = require('./routes/admin.js');
+var users = require('./routes/users.js')
 
 app.use(cors());
 app.use(main.routes());
 app.use(admin.routes());
+app.use(users.routes());
 
 
 var port = process.env.PORT || 3000;
